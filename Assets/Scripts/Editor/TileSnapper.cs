38aacc07bd241032bc80c027732523c745602e58
Marcin Przybyłek <marcin.przybylek175@gmail.com>
2025-11-25 04:35:23 +0100

    - Added snappable ladder and half-ladder

--- Assets/Scripts/Editor/TileSnapper.cs
+++ Assets/Scripts/Editor/TileSnapper.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed5d2f308444a39e43ab54df1786767378e7b52ba1fd8651b7c7a516205bda4b
-size 2250
+oid sha256:2dfbeb5c5fc6cc17fb8d0d77baa05d8478580c3269ee960b91a9c5f9bed6ee64
+size 2243
