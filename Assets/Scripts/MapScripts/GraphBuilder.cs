2d3c2427851db9139c9d0ca62446debf65958cb6
Marcin Przybyłek <marcin.przybylek175@gmail.com>
2026-02-17 01:33:38 +0100

    - Optimized models used in game - Optimized lighting - Added preprocessor directives for Unity Editor only code

--- Assets/Scripts/MapScripts/GraphBuilder.cs
+++ Assets/Scripts/MapScripts/GraphBuilder.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f91fcf43b486eff39090b000e0116c72090e93f0a17613f5ba69040cbf26ff5
-size 9200
+oid sha256:965355dc9cdfdea7952af1eb1c4ad9edbc48167365afda75d138ff9983e767cd
+size 9116
--- Assets/Scripts/MapScripts/Node.cs
+++ Assets/Scripts/MapScripts/Node.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6fe53ac5981b04d1ba4e82f00b159bc4da891dab073eb25605b1c1adaecf46c
-size 996
+oid sha256:e58e2469435ab4ce217d461c07806c7998b2b271ab72c9a8c671c7efdf41fb80
+size 1024
