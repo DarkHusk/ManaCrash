86683f09f4c58b8056637f5e948c74c61cf399ce
DarkHusk <marcin.przybylek175@gmail.com>
2026-02-25 20:48:18 +0100

    - Fixed an issue causing active units highlight to disappear

--- Assets/Scripts/Entities/Controllers/PlayerController.cs
+++ Assets/Scripts/Entities/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddbea544c23c16f4d919054e4c1fd77521a0d870868317cb3f231b35add44982
-size 7312
+oid sha256:e66a2e772aee41e84e3070dfc3e9e0adcde35b7f04cbe58d600722aad03ac302
+size 7249
